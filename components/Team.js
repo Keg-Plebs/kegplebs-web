cce30e762c662b031019336c108a91ac089a1712
loganorndorf <orndorf18@up.edu>
2021-12-27 10:45:40 -0800

    Simple readabiliity improvement

--- components/Team.js
+++ components/Team.js
@@ -9,7 +9,7 @@ import shdw from '../public/bitchass.jpeg';
 import jefftheworm from '../public/jefftheworm.jpeg';
 
 import sectionStyles from '../styles/Section.module.css';
-import styles from '../styles/Team.module.css';
+import { team, container, card, img, heading, name, twitter, title } from '../styles/Team.module.css';
 
 const Team = () => {
     
@@ -17,60 +17,60 @@ const Team = () => {
 
 
     return(
-        <div className={`${sectionStyles.main} ${styles.team}`}>
+        <div className={`${sectionStyles.main} ${team}`}>
             <h1>THE KEG PLEB SCIENTISTS</h1>
-            <div className={styles.container}>
-                <div className={styles.card} id="sj">
-                    <div className={styles.img}>
+            <div className={container}>
+                <div className={card} id="sj">
+                    <div className={img}>
                         <Image src={sj} quality={100} width={size} height={size}></Image>
                     </div>
-                    <div className={styles.heading}>
-                        <h3 className={styles.name}>SJ</h3>
-                        <a href='https://twitter.com/Sjams_'><FontAwesomeIcon icon={faTwitter} className={styles.twitter}/></a>
+                    <div className={heading}>
+                        <h3 className={name}>SJ</h3>
+                        <a href='https://twitter.com/Sjams_'><FontAwesomeIcon icon={faTwitter} className={twitter}/></a>
                     </div>
-                    <p className={styles.title}>MARKETING / CO-FOUNDER</p>
+                    <p className={title}>MARKETING / CO-FOUNDER</p>
                 </div>
-                <div className={styles.card} id="oozic">
-                    <div className={styles.img}>
+                <div className={card} id="oozic">
+                    <div className={img}>
                         <Image src={oozic} quality={100} width={size} height={size}></Image>
                     </div>
-                    <div className={styles.heading}>
-                        <h3 className={styles.name}>IZAAC</h3>
-                        <a href='https://twitter.com/izaactoonks'><FontAwesomeIcon icon={faTwitter} className={styles.twitter}/></a>
+                    <div className={heading}>
+                        <h3 className={name}>IZAAC</h3>
+                        <a href='https://twitter.com/izaactoonks'><FontAwesomeIcon icon={faTwitter} className={twitter}/></a>
                     </div>
-                    <p className={styles.title}>ART LEAD / CO-FOUNDER</p>
+                    <p className={title}>ART LEAD / CO-FOUNDER</p>
                 </div>
-                <div className={styles.card} id="tsiz">
-                    <div className={styles.img}>
+                <div className={card} id="tsiz">
+                    <div className={img}>
                         <Image src={tsiz} quality={100} width={size} height={size}></Image>
                     </div>
-                    <div className={styles.heading}>
-                        <h3 className={styles.name}>TSIZ</h3>
-                        <a href='https://twitter.com/tsiz_eth'><FontAwesomeIcon icon={faTwitter} className={styles.twitter}/></a>
+                    <div className={heading}>
+                        <h3 className={name}>TSIZ</h3>
+                        <a href='https://twitter.com/tsiz_eth'><FontAwesomeIcon icon={faTwitter} className={twitter}/></a>
                     </div>
-                    <p className={styles.title}>ADVISOR</p>
+                    <p className={title}>ADVISOR</p>
                 </div>
             </div>
-            <div className={styles.container}>
-                <div className={styles.card} id="shdw">
-                    <div className={styles.img}>
+            <div className={container}>
+                <div className={card} id="shdw">
+                    <div className={img}>
                         <Image src={shdw} quality={100} width={size} height={size}></Image>
                     </div>
-                    <div className={styles.heading}>
-                        <h3 className={styles.name}>SHDW</h3>
-                        <a href='https://twitter.com/shdw_dev'><FontAwesomeIcon icon={faTwitter} className={styles.twitter}/></a>
+                    <div className={heading}>
+                        <h3 className={name}>SHDW</h3>
+                        <a href='https://twitter.com/shdw_dev'><FontAwesomeIcon icon={faTwitter} className={twitter}/></a>
                     </div>
-                    <p className={styles.title}>DEVELOPER</p>
+                    <p className={title}>DEVELOPER</p>
                 </div>
-                <div className={styles.card} id="jefftheworm">
-                    <div className={styles.img}>
+                <div className={card} id="jefftheworm">
+                    <div className={img}>
                         <Image src={jefftheworm} quality={100} width={size} height={size}></Image>
                     </div>
-                    <div className={styles.heading}>
-                        <h3 className={styles.name}>JEFFTHEWORM</h3>
-                        <a href='https://twitter.com/JefftheWorm'><FontAwesomeIcon icon={faTwitter} className={styles.twitter}/></a>
+                    <div className={heading}>
+                        <h3 className={name}>JEFFTHEWORM</h3>
+                        <a href='https://twitter.com/JefftheWorm'><FontAwesomeIcon icon={faTwitter} className={twitter}/></a>
                     </div>
-                    <p className={styles.title}>DEVELOPER</p>
+                    <p className={title}>DEVELOPER</p>
                 </div>
             </div>
         </div>
