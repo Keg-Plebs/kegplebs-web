6451d275a9b225b2249603883c2cde946b18d206
loganorndorf <orndorf18@up.edu>
2022-01-04 12:51:01 -0800

    Navbar changes made

--- public/images.js
+++ public/images.js
@@ -8,3 +8,7 @@ export { default as Hipp } from './hipp.PNG';
 export { default as Vort } from './vort.jpg';
 export { default as Space } from './space.JPG';
 export { default as Hoodee } from './hoodee.JPG';
+export { default as NavIcon } from './KegPlebSun.ico';
+export { default as Opensea } from './openseaicon.png';
+export { default as NeonSign } from './neonSign.png';
+export { default as Etherscan } from './etherscan.png';
