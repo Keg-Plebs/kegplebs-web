f8d8be34aa078ad21aad80c7977b7557c50a12c4
loganorndorf <orndorf18@up.edu>
2022-01-20 10:24:59 -0800

    Added link scrolling along the page; using react-scroll module

--- components/Nav.js
+++ components/Nav.js
@@ -1,5 +1,7 @@
 import Link from 'next/link';
 import Image from 'next/image';
+import Scroll, { scroller } from 'react-scroll';
+const Scroller = Scroll.scroller;
 
 import { faTwitter, faDiscord } from '@fortawesome/free-brands-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -11,20 +13,54 @@ import Layout from './Layout';
 import Connect from './Connect';
 
 const Nav = () => {
+    const toBrewverse = () => {
+        scroller.scrollTo('brewverse', {
+            duration: 500,
+            smooth: true,
+            offset: 100  
+        })
+    }
+
+    const toAbout = () => {
+        scroller.scrollTo('about', {
+            duration: 500,
+            smooth: true,
+            offset: -100
+        })
+    }
+
+    const toTeam = () => {
+        scroller.scrollTo('team', {
+            duration: 500,
+            smooth: true
+        })
+    }
+
+    const toTop = () => {
+        scroller.scrollTo('top', {
+            duration: 500,
+            smooth: true
+        })
+    }
+
+
+
     return (
         <nav className={nav}>
                 <div className={logoGroup}>
-                    <Image src={NeonSign} quality={100} priority={true} height={75} width={350}></Image>
+                    <a onClick={toTop}>
+                        <Image src={NeonSign} quality={100} priority={true} height={75} width={350}></Image>
+                    </a>
                 </div>
                 <ul className={linkGroup}>
                     <li className={li}>
-                        <span className={link}><Link href="/">BREWVERSE</Link></span>
+                        <span className={link}><a onClick={toBrewverse} href="#">BREWVERSE</a></span>
                     </li>
                     <li className={li}>
-                        <span className={link}><Link href="/">ABOUT</Link></span>
+                        <span className={link}><a onClick={toAbout} href="#">ABOUT</a></span>
                     </li>
                     <li className={li}>
-                        <span className={link}><Link href="/">TEAM</Link></span>
+                        <span className={link}><a onClick={toTeam} href="#">TEAM</a></span>
                     </li>
                 </ul>
                 <div className={socialGroup}>
