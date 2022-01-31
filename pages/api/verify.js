f9759436e1990e92deeadc10149ec02c4e12a870
Kai Vickers <kaiv14@comcast.net>
2022-01-31 02:38:37 -0800

    Implemented wallet authentication
    When a user connects their wallet, a nonce is generated on the server
    and stored in a gun datastore. On the client this nonce is signed by the
    user, with the signature being used to verify whether the address of the
    wallet matches that of the sigee.
    
    Each time the user connects a new nonce is generated and stored on the
    backend. Will likely need to refactor the datastore to make sure it is
    updating the data properly.

---
+++ pages/api/verify.js
@@ -0,0 +1,36 @@
+import { ethers } from 'ethers'
+import { gun } from './services/database'
+
+// https://www.youtube.com/watch?v=tVyQZ0CiMWI
+// https://mirror.xyz/sha.eth/i6ry1Mxez53z91ef375sMe2rO1NvK2ipACyzKA4SR9g
+
+// Signature verification API
+const verify = (req, res) => {
+
+    // Initially set verification to false
+    let verified = false;
+
+    // Receive the posted address and signature
+    const { address, signature } = req.body;
+
+    // Get the nonce from the data base for the address
+    gun.get('root').get('users').get(address).get('nonce').once((nonce) => {
+
+        // Creates the message that was signed on the client
+        const message = "Sign this nonce to connect: " + nonce;
+
+        // Verifies the signature by returning the address of the signee
+        const signerAddress = ethers.utils.verifyMessage(message, signature);
+
+        // If the signature address matches the posted one, the wallet is verified.
+        if (address == signerAddress) {
+            verified = true;
+        }
+
+        // Send the verification back to the client
+        res.status(200).json({ verified });
+    });
+
+}
+
+export default verify
