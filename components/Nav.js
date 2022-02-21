e37a9de2ecc940a40872575235b1cd48ca27ff4a
loganorndorf <orndorf18@up.edu>
2022-02-21 10:55:31 -0800

    Updating Discord links

--- components/Nav.js
+++ components/Nav.js
@@ -158,7 +158,7 @@ const Nav = ({enabled}) => {
                     >
                         <div className={icons}>
                             <a className={a} href='https://twitter.com/KegPlebs'><FontAwesomeIcon icon={faTwitter} className={socialIcons}/></a>
-                            <a className={a} href='https://discord.gg/xjfpxWajXH'><FontAwesomeIcon icon={faDiscord} className={socialIcons}/></a>
+                            <a className={a} href='https://discord.gg/zEWcPMS7Uf'><FontAwesomeIcon icon={faDiscord} className={socialIcons}/></a>
                             <a className={a} href='#'><Image src={Opensea} quality={100} height={30} width={30}></Image></a>
                             <a className={a} href='#'><Image src={Etherscan} quality={100} height={30} width={30}></Image></a>
                         </div>
