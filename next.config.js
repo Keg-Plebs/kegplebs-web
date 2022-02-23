1a98de272ca5e863e06989dae6a6e3bb1e224e6a
Kai Vickers <kaiv14@comcast.net>
2022-02-23 03:08:20 -0800

    removed env variable exports

--- next.config.js
+++ next.config.js
@@ -4,11 +4,11 @@ const options = {
     formats: ['image/webp'],
     domains: ['https://dspp4aqyru269.cloudfront.net'],
   },
-  env: {
-    ACCESS_KEY_ID: process.env.AWS_ACCESS_KEY_ID,
-    S3_BUCKET: process.env.AWS_S3_BUCKET,
-    SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY
-  }
+  // env: {
+  //   ACCESS_KEY_ID: process.env.AWS_ACCESS_KEY_ID,
+  //   S3_BUCKET: process.env.AWS_S3_BUCKET,
+  //   SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY
+  // }
 }
 
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
