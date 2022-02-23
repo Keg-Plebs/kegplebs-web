ba2ef7098af378751c66a9f77ab162f9d4420193
Kai Vickers <kaiv14@comcast.net>
2022-02-23 03:09:53 -0800

    Removed env variables from module.exports

--- next.config.js
+++ next.config.js
@@ -4,11 +4,11 @@ const options = {
     formats: ['image/webp'],
     domains: ['https://dspp4aqyru269.cloudfront.net'],
   },
-  env: {
-    SUPABASE_URL: process.env.SUPABASE_URL,
-    SUPABASE_KEY: process.env.SUPABASE_KEY,
-    JWT_SECRET: process.env.JWT_SECRET
-  }
+  // env: {
+  //   SUPABASE_URL: process.env.SUPABASE_URL,
+  //   SUPABASE_KEY: process.env.SUPABASE_KEY,
+  //   JWT_SECRET: process.env.JWT_SECRET
+  // }
 }
 
 // const withBundleAnalyzer = require('@next/bundle-analyzer')({
