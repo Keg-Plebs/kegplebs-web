4a099fe9929d45067e52894947519fb2ca325825
loganorndorf <orndorf18@up.edu>
2022-02-23 20:00:46 -0800

    Trying to find the right way to signTransactions

--- components/Bar.js
+++ components/Bar.js
@@ -730,13 +730,16 @@ const Bar = props => {
     const [doorState, setDoorState] = useState(false);
     const [bartenderClicked, setBartenderClicked] = useState(false);
     const [cheersScreen, setCheersScreen] = useState(false);
-    const [mintAvailable, setMintAvailable] = useState(true);
+    const [mintAvailable, setMintAvailable] = useState(false);
     const [mintScreen, setMintScreen] = useState(false);
     const [warning, setWarning] = useState(false);
-    const [allowlistPeriod, setAllowlistPeriod] = useState(false);
+    const [isAllowlistPeriod, setAllowlistPeriod] = useState(false);
 
     useEffect( () => {
         document.body.style.overflow = 'hidden';
+
+        // check if wallet is connected - this data needs to persist when we move from scene to scene
+
 
         return () => {
             document.body.style.overflow = 'visible';
@@ -757,22 +760,24 @@ const Bar = props => {
             return;
         } else {
             // connect to contract -> check public paused
-            const contract = await new ethers.Contract(contractAddress, contractAbi, provider);
+            const contract = new ethers.Contract(contractAddress, contractAbi, provider);
             const paused = await contract.paused();
             const isAllow = await contract.allowlistMintPeriod();
-            setMintAvailable(paused);
-            setAllowlistPeriod(isAllow);
+            
+            setMintAvailable(!paused); // for displaying html
+            setAllowlistPeriod(isAllow); // for props
+
+            setBartenderClicked(true);
+
+            if(!paused) {
+                setTimeout(() => {
+                    setMintScreen(true);
+                    setBartenderClicked(false);
+                }, 3000);
+            } 
         }
-
-        setBartenderClicked(true);
-
-        if(mintAvailable) {
-            setTimeout(() => {
-                setMintScreen(true);
-                setBartenderClicked(false);
-            }, 3000);
-        } 
     }
+
 
     const handleMintScreenExit = () => {
         setMintScreen(false);
@@ -836,7 +841,7 @@ const Bar = props => {
                 }
                 {
                     mintScreen ? 
-                    <Dapp exitMint={handleMintScreenExit} allowPeriod={allowlistPeriod}></Dapp> :
+                    <Dapp exitMint={handleMintScreenExit} allowPeriod={isAllowlistPeriod}></Dapp> :
                     <></>
                 }
                 {
